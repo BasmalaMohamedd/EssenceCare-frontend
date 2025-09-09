759188d051d4d7513d4f9920a2635c8355142dc6
BasmalaMohamedd <basmalamohamed934@gmail.com>
2025-09-09 20:59:00 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar />
+    <Home />
+    </>
+    
+  )
+}
+
+export default App
