437e7b980ddec984407bec027f1baa82521ca43a
BasmalaMohamedd <basmalamohamed934@gmail.com>
2025-09-16 23:49:37 +0300

    light mode bug fix

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = () => {
             </a>
             <div className="px-5 pb-5">
                 <a href="#">
-                    <h5 className="text-l font-semibold tracking-tight text-gray-900 dark:text-white">Moisterizer</h5>
+                    <h5 className="text-l font-semibold tracking-tight text-gray-900 ">Moisterizer</h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
                     <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -31,7 +31,7 @@ const ProductCard = () => {
                     <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-xl font-bold text-gray-900 ">$599</span>
                     <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-1 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
                 </div>
             </div>
