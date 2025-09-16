25be30f09955eaa9a2ed3d59da168f59cb5252e0
BasmalaMohamedd <basmalamohamed934@gmail.com>
2025-09-16 23:46:20 +0300

    undo mobile changes

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -4,9 +4,9 @@ import ProductCard from './ProductCard'
 const BestSellers = () => {
   return (
     <div>
-      <section className='md:m-5'>
+      <section className='m-5'>
         <h2 className='text-center text-3xl'>OUR BEST SELLERS</h2>
-        <div className='m-10 flex border border-gray-200 p-3 overflow-auto'>
+        <div className='md:m-10 flex border border-gray-200 p-3 overflow-auto'>
             <ProductCard />
             <ProductCard />
             <ProductCard />
