d072a45ac44590c032bc7bb9379d409e4101dbd6
BasmalaMohamedd <basmalamohamed934@gmail.com>
2025-10-07 20:16:26 +0300

    style bug fix

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -5,7 +5,7 @@ const AllProducts = () => {
   return (
     <div className='p-3 '>
         <h1 className='text-xl ml-4 text-center m-2 font-light text-[#987070]'>Shop/ <span className='text-2xl font-bold text-center text-[#987070]'>All Products</span></h1>
-        <section className='p-5'>
+        <section className='md:p-7'>
         <div className='flex justify-around flex-wrap md:space-x-20 p-2 '>
             <ProductCard />
             <ProductCard />
